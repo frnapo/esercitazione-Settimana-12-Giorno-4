78f061cab748542e37255ec32a8cd66dc4423d28
Francesco <napo.fra96@gmail.com>
2024-02-01 15:27:39 +0100

    es-settimana-12-giorno-4

---
+++ es-Settimana-12-Giorno-4/es-Settimana-12-Giorno-4/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace es_Settimana_12_Giorno_4
+{
+    internal class Program
+    {
+
+        static bool esci = false;
+        static void Main(string[] args)
+        {
+            while (!esci)
+            {
+                Console.WriteLine("===============OPERAZIONI==============");
+                Console.WriteLine("1. Login");
+                Console.WriteLine("2. Logout");
+                Console.WriteLine("3. Verifica ora e data di login");
+                Console.WriteLine("4. Lista degli accessi");
+                Console.WriteLine("5. Esci");
+                Console.WriteLine("=======================================");
+                string scelta = Console.ReadLine();
+                switch (scelta)
+                {
+                    case "1":
+                        Utente.Login();
+                        break;
+                    case "2":
+                        Utente.Logout();
+                        break;
+                    case "3":
+                        Utente.VerificaDataOraLogin();
+                        break;
+                    case "4":
+                        Utente.ListaAccessi();
+                        break;
+                    case "5":
+                        esci = true;
+                        break;
+                    default:
+                        Console.WriteLine("Scelta non valida!");
+                        break;
+                }
+            }
+
+        }
+    }
+}
+
+
+
---
+++ es-Settimana-12-Giorno-4/es-Settimana-12-Giorno-4/Utente.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace es_Settimana_12_Giorno_4
+{
+    public static class Utente
+    {
+        private static string username { get; set; }
+        private static string password { get; set; }
+        private static string confirmPassword { get; set; }
+        private static bool isLogged = false;
+
+        // list date time crea array di date
+        private static DateTime loginTime { get; set; }
+        private static List<DateTime> accessi = new List<DateTime>();
+
+
+        public static void Login()
+        {
+            if (isLogged)
+            {
+                Console.WriteLine("Utente gia' loggato!");
+                return;
+            }
+            else
+            {
+                Console.WriteLine("Inserisci username");
+                string username = Console.ReadLine();
+                Console.WriteLine("Inserisci password");
+                string password = Console.ReadLine();
+                Console.WriteLine("Inserisci conferma password");
+                string confirmPassword = Console.ReadLine();
+                if (username != null && password == confirmPassword)
+                {
+                    Console.WriteLine("Login effettuato con successo!");
+                    loginTime = DateTime.Now;
+                    //login time prende la data e ora corrente
+                    accessi.Add(loginTime);
+                    //aggiunge la data e ora corrente alla lista accessi
+                    isLogged = true;
+                }
+                else
+                {
+                    Console.WriteLine("Login fallito, riprovare.");
+                }
+            }
+        }
+
+        public static void Logout()
+        {
+            if (isLogged)
+            {
+                Console.WriteLine("Vuoi effettuare il logout? (y/n)");
+                string risposta = Console.ReadLine();
+                if (risposta == "y")
+                {
+                    Console.WriteLine("Logout effettuato con successo!");
+                    username = null;
+                    password = null;
+                    isLogged = false;
+                }
+                else
+                {
+                    Console.WriteLine("Logout annullato!");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Utente non loggato!");
+            }
+        }
+
+        public static void VerificaDataOraLogin()
+        {
+            if (isLogged)
+            {
+                Console.WriteLine($"Data e ora di login per {username}: {loginTime}");
+            }
+            else
+            {
+                Console.WriteLine("Utente non loggato!");
+            }
+        }
+
+        public static void ListaAccessi()
+        {
+            if (isLogged)
+            {
+                Console.WriteLine($"Lista degli accessi:");
+                foreach (DateTime accesso in accessi) // per ciascun elemento (accesso in questo caso)
+                                                      // presente nell'array di date, mostra in console
+                {
+                    Console.WriteLine(accesso);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Utente non loggato!");
+            }
+        }
+
+
+    }
+}
